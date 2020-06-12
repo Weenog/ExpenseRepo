5e4cbc7c917760012fe4601ed47255c4cd7e0d25
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-12 10:36:42 +0200

    page running, no errors

--- ExpenseApp/Startup.cs
+++ ExpenseApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExpenseApp.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -21,11 +22,11 @@ namespace ExpenseApp
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddControllersWithViews();
-        //    services.AddSingleton<IExpenseDatabase, ExpenseDatabase>();
-        //}
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddSingleton<IExpenseDatabase, ExpenseDatabase>();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
