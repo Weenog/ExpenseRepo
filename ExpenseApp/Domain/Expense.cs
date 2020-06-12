f018e40c709531e7f16293ca2765cfad0b4e5775
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-12 11:02:44 +0200

    set was internal (fixed)

--- ExpenseApp/Domain/Expense.cs
+++ ExpenseApp/Domain/Expense.cs
@@ -7,9 +7,9 @@ namespace ExpenseApp.Domain
 {
     public class Expense
     {
-        public int Id { get; internal set; }
-        public object Amount { get; internal set; }
-        public object Description { get; internal set; }
-        public object Date { get; internal set; }
+        public int Id { get; set; }
+        public object Amount { get; set; }
+        public object Description { get; set; }
+        public object Date { get; set; }
     }
 }
