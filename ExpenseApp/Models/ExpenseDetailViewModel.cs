8606f9961c26eef89bd1463965976ab22df91b17
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-12 10:59:00 +0200

    Views++

--- ExpenseApp/Models/ExpenseDetailViewModel.cs
+++ ExpenseApp/Models/ExpenseDetailViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace ExpenseApp.Controllers
+namespace ExpenseApp.Models
 {
     public class ExpenseDetailViewModel
     {
