61a46339037608d5bc0921a51d2a2e6a551a7f6f
Thomas Vanhuffel <thomasvanhuffel@hotmail.com>
2020-06-15 10:06:14 +0200

    seemsokay

--- ExpenseApp/Models/ExpenseDeleteViewModel.cs
+++ ExpenseApp/Models/ExpenseDeleteViewModel.cs
@@ -7,6 +7,7 @@ namespace ExpenseApp.Models
 {
     public class ExpenseDeleteViewModel
     {
+
         public int Id { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
